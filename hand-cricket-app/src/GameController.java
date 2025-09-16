fa548bff57902c9ca6ee34d237ab42931c55a1df
AMP26 <amit.patnaik0126@gmail.com>
2025-09-16 23:48:33 +0530

    Hand Cricket

--- hand-cricket-app/src/GameController.java
+++ hand-cricket-app/src/GameController.java
@@ -27,7 +27,7 @@ public class GameController {
     public Scene initUI() {
         logic = new GameLogic(this);         
 
-        root.setPadding(new Insets(25));
+        root.setPadding(new Insets(25));    
         root.setAlignment(Pos.CENTER);
         statusLabel.setFont(Font.font("Arial", 18));
         scoreLabel.setFont(Font.font("Arial", 22));
@@ -36,7 +36,7 @@ public class GameController {
 
         restartButton.setOnAction(e -> logic.resetGame());
         buttonRow.setAlignment(Pos.CENTER);    
-     
+
         HBox handRow = new HBox(50);
         handRow.setAlignment(Pos.CENTER);
 
